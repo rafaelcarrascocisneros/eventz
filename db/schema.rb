21dfe936a2e9a048c02f399be152101b0cd62fa1
Rafael Carrasco Cisneros <rcarrascocisne9122@sdsu.edu>
2023-12-22 12:07:43 -0800

    One-to-Many:belongs_to (Lesson 21)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_033635) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_22_194712) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -23,4 +23,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_033635) do
     t.integer "capacity", default: 1
   end
 
+  create_table "registrations", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "how_heard"
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_registrations_on_event_id"
+  end
+
+  add_foreign_key "registrations", "events"
 end
